b8e9cae52e605ec06768a26887488f0c7ca6ec7e
sbpark88 <saebyulpark88@gmail.com>
2024-07-04 13:18:20 +0900

    Close #1: Add top nav bar layout

---
+++ app/ui/topnav.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+import useDebounceFn from "@/app/utils/useDebounceFn";
+
+export default function TopNav() {
+  const [open, setOpen] = useState(false);
+  const closeNav = useDebounceFn(() => {
+    if (window.innerWidth >= 768) {
+      setOpen(false);
+    }
+  });
+
+  useEffect(() => {
+    window.addEventListener("resize", closeNav);
+    return () => window.removeEventListener("resize", closeNav);
+  }, [closeNav]);
+
+  return (
+    <nav className="relative h-14 z-40 w-full text-white bg-orange-400">
+      <div className="h-full flex justify-between items-center flex-col md:flex-row">
+        <div
+          className={clsx(
+            "px-5 sm:px-10 h-full w-full flex justify-between items-center text-2xl bg-orange-400 z-50 md:w-auto",
+            {
+              "border-b-2": open,
+            },
+          )}
+        >
+          <Link href="/">Logo</Link>
+          <button
+            className="md:hidden text-4xl"
+            onClick={() => setOpen((prev) => !prev)}
+          >
+            {open ? "-" : "+"}
+          </button>
+        </div>
+        <div
+          className={clsx(
+            "absolute bg-orange-400 left-0 top-0 right-0 duration-300 transform-gpu md:static md:visible md:opacity-100 md:pr-10",
+            open
+              ? "translate-y-14 visible opacity-100"
+              : "translate-y-0 invisible opacity-0",
+          )}
+        >
+          <div
+            className={clsx(
+              "flex flex-col items-center content-center md:block",
+            )}
+          >
+            <NavLinks />
+            <button className="py-2 md:ml-5">Sign Out</button>
+            <button className="py-2 md:ml-5">Sign In</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+const links = [
+  { name: "Admin", href: "/admin" },
+  { name: "User", href: "/user" },
+];
+
+function NavLinks() {
+  return (
+    <>
+      {links.map((link) => (
+        <Link key={link.name} href={link.href} className="py-2 md:ml-5">
+          {link.name}
+        </Link>
+      ))}
+    </>
+  );
+}
